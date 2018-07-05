eeb8e7d79abc6334e6a6df2ecf3d93a08ec4fef9
yusuke.kimura <yusuke.a.kimura@rakuten.com>
2018-07-05 06:13:39 +0000

    toko

---
+++ resources/views/post/post.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app') 
+
+@section('content')
+
+<div class="row">
+        <div class="col-xs-6">
+    {!! Form::model($post, ['route' => 'posts.store']) !!}
+        <div class="form-group">
+        {!! Form::label('restaurant', '店名:') !!}
+        {!! Form::text('restaurant', null, ['class' => 'form-control']) !!}
+         </div>
+         <div class="form-group">        
+        {!! Form::label('cost', '値段:') !!}
+        {!! Form::text('cost', null, ['class' => 'form-control']) !!}
+         </div>
+        <div class="form-group">
+        {!! Form::label('friends', '同行者:') !!}
+        {!! Form::text('friends', null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+        {!! Form::label('went_at', '日にち:') !!}
+        {!! Form::date('went_at', null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+        {!! Form::label('comments', 'コメント:') !!}
+        {!! Form::text('comments', null, ['class' => 'form-control']) !!}
+        </div>
+
+        {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+
+    {!! Form::close() !!}
+  </div>
+</div>
+
+@endsection
