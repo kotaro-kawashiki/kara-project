d15f058d3a1112699f157b0472b6de174dec3f24
kotaro.kawashiki <kotaro.kawashiki@rakuten.com>
2018-07-09 03:13:45 +0000

    july 9

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
-        <!--this file is a common object of view pages and navbar-->
+        <!--this file is a common object of view pages-->
 
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="ja">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,10 +10,9 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Kara-Project') }}</title>
+    <title>Kara-Project</title>
 
     <!-- Styles -->
-    <!--<link href="{{ asset('css/app.css') }}" rel="stylesheet">-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
