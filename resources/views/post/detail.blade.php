ab7bf6e9d2c81595b59abc8e514e73bddc270360
kotaro.kawashiki <kotaro.kawashiki@rakuten.com>
2018-07-09 04:26:35 +0000

    adjust grid of detail page

--- resources/views/post/detail.blade.php
+++ resources/views/post/detail.blade.php
@@ -2,11 +2,25 @@
 
 @section('content')
 
-<img src="/image/penguin.jpg" alt="/image/penguin.jpg">
-<div>{{$post->restaurant}}
+<div class="col-xs-12 col-sm-5 col-md-5 col-lg-5">
+     <img src="/image/penguin.jpg" class="img-responsive" alt="/image/penguin.jpg">
+</div>
+<div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
+     <div class="media-heading">
+     {{$post->restaurant}}
      {{$post->cost}}円
      {{$post->went_at}}
      {{$post->friends}}
-     {{$post->comments}}</div>
+     {{$post->comments}}
+     </div>
+</div>     
+
+
+
+
+
+
+
+     
 
 @endsection
