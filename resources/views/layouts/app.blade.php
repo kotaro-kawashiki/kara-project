7a1d6e114c2d5a4b4b0ddbc03dd6491a37cee059
kotaro.kawashiki <kotaro.kawashiki@rakuten.com>
2018-07-09 06:25:00 +0000

    card design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
         <!--this file is a common object of view pages-->
 
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,6 +13,7 @@
     <title>Kara-Project</title>
 
     <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
--- resources/views/post/detail.blade.php
+++ resources/views/post/detail.blade.php
@@ -3,10 +3,11 @@
 @section('content')
 
 <div class="col-xs-12 col-sm-5 col-md-5 col-lg-5">
+     
      <img src="/image/penguin.jpg" class="img-responsive" alt="/image/penguin.jpg">
 </div>
 <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
-     <div class="media-heading">
+     <div class="">
      {{$post->restaurant}}
      {{$post->cost}}円
      {{$post->went_at}}
