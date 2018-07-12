25fa3ee12139a2ae52483e3b955fc4931b4a6ef5
ayame.murayama <ayame.murayama@rakuten.com>
2018-07-12 07:44:58 +0000

    0712

--- resources/views/post/timeline.blade.php
+++ resources/views/post/timeline.blade.php
@@ -1,23 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-{!! Form::open(['method' => 'GET']) !!}
-  {!! Form::text('s',null) !!}
-  {!! Form::submit('検索') !!}
-{!! Form::close() !!}
+
+<!--検索ボタンのコーナー-->
+<div id="kensaku" class="row">
+      <div class="text-center">
+          {!! Form::open(['method' => 'get', 'class' => 'form-inline']) !!}
+          <div class="form-group">
+          {!! Form::text('s',null, ['class' => 'form-control input-lg']) !!}
+          </div>
+          {!! Form::submit('検索', ['class' => 'btn btn-success btn-lg']) !!}
+          {!! Form::close() !!}
+      </div>
+</div>
+
+<!--タイムラインのカード-->
   @foreach ($data as $post)
-  <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+  <div id="timeline" class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
       <div class="thumbnail" id="{{$post->went_at}}">
-        <caption style="text-align:right;">{{$post->went_at}}</caption>
-        <img src="/image/penguin.jpg" alt="/image/penguin.jpg">
+        <center><caption style="text-align:right;"><h2>{{$post->went_at}}</h2></caption></center> <!--いった日付-->
+        
+        <img class="img-rounded" src="/image/gohan.jpg" alt="/image/gohan.jpg">
         <div class="caption">
-          <h3><span class="glyphicon glyphicon-cutlery"></span>:{{$post->restaurant}}</h3>
-          <h4><span class="glyphicon glyphicon-credit-card"></span>:{{$post->cost}}円</h4>
-            <a href="{{ route('posts.show',['id' => $post->id]) }}"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></a>
+          <h2><center><span class="glyphicon glyphicon-cutlery"></span> {{$post->restaurant}}　  
+              <span class="glyphicon glyphicon-yen"></span>{{$post->cost}}</center></h2>
+            <center>  
+            <h2><a href="{{ route('posts.show',['id' => $post->id]) }}"><span class="glyphicon glyphicon-option-horizontal" aria-hidden="true"></span></a>
             <a href="{{ route('posts.edit',['id' => $post->id]) }}"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>
-            @include('user_favo.favo_button')
-            <a href="#"><span class="glyphicon glyphicon-heart"></span></a>
+            <a href="#"><span class="glyphicon glyphicon-heart"></span></a></h2>
+            </center>
             @include('user_favo.favo_button')
         </div>
       </div>
--- resources/views/user_favo/favo_button.blade.php
+++ resources/views/user_favo/favo_button.blade.php
@@ -1,11 +1,12 @@
     @if (Auth::user()->is_favoriting($post->id))
         {!! Form::open(['route' => ['user.unfavo', $post->id], 'method' => 'delete']) !!}
-            {!! Form::submit('Unfavo') !!}
+            {!! Form::submit('♥',['class' => 'btn btn-default btn-lg']) !!}
             
         {!! Form::close() !!}
     @else
         {!! Form::open(['route' => ['user.favo', $post->id]]) !!}
-            {!! Form::submit('Favo') !!}
+            {!! Form::submit('♡',['class' => 'btn btn-default btn-lg']) !!}
             
         {!! Form::close() !!}
     @endif
+
