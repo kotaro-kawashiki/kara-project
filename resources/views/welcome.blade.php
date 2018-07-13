36c4a70388ac76f9ba129df612b56195c2bcc98e
ayame.murayama <ayame.murayama@rakuten.com>
2018-07-13 00:32:00 +0000

    0713

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    めし日記
+                    kara-project
                 </div>
 
                 
