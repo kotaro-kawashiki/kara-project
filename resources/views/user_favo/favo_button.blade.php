81265bf3c34ec091ffcd52dd8616dba499f1a256
ayame.murayama <ayame.murayama@rakuten.com>
2018-07-18 03:43:07 +0000

    mergeshitai

--- resources/views/user_favo/favo_button.blade.php
+++ resources/views/user_favo/favo_button.blade.php
@@ -1,11 +1,11 @@
     @if (Auth::user()->is_favoriting($post->id))
         {!! Form::open(['route' => ['user.unfavo', $post->id], 'method' => 'delete']) !!}
-            {!! Form::submit('お気に入りにする',['class' => 'btn btn-default btn-lg']) !!}
+            {!! Form::submit('お気に入りにする',['class' => 'btn btn-warning btn-lg']) !!}
             
         {!! Form::close() !!}
     @else
         {!! Form::open(['route' => ['user.favo', $post->id]]) !!}
-            {!! Form::submit('お気に入りをやめる',['class' => 'btn btn-warning btn-lg']) !!}
+            {!! Form::submit('お気に入りをやめる',['class' => 'btn btn-default btn-lg']) !!}
             
         {!! Form::close() !!}
     @endif
