74f49045a4e26452a567fd2176c9b7d467c931ed
natsumi.hironaka <natsumi.hironaka@rakuten.com>
2018-07-18 01:28:49 +0000

    errocomplete

--- resources/views/commons/error_messages.blade.php
+++ resources/views/commons/error_messages.blade.php
@@ -1,3 +1,6 @@
 @if (count($errors) > 0)
-        <div class="alert alert-warning col-lg-6 col-xs-12">コメント欄以外は必須です！</div>
+            <br>
+            <br>
+            <br>
+       <div class="alert alert-warning col-xs-12 col-lg-offset-4 col-lg-4 ">コメント欄以外は必須です！</div>
 @endif
