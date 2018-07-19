1b8e412d9872fab73b20e211e4176b40cf34366e
tomohiro.sai <tomohiro.sai@rakuten.com>
2018-07-19 03:55:09 +0000

    タイムライン完成

--- resources/views/post/timeline.blade.php
+++ resources/views/post/timeline.blade.php
@@ -3,13 +3,13 @@
 @section('content')
 
 <div class = "grade">
-<div class="col-xs-12 col-sm-5 col-md-5 col-lg-offset-3 col-lg-6"4169e1>
-<h1>投稿一覧</h1>
+<div class="col-xs-12 col-sm-5 col-md-5 col-lg-offset-2 col-lg-8"4169e1><center>
+<h1>投稿一覧</h1></center>
 </div>
 
 
   <!--search form-->
-<div class="col-xs-12 col-sm-5 col-md-5 col-lg-offset-3 col-lg-6">
+<div class="col-xs-12 col-sm-5 col-md-5 col-lg-offset-3 col-lg-6"><center>
 <form method="GET" class="form-inline">
   <div class="form-group">
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="margin-bottom:10px;">
@@ -34,7 +34,7 @@
   </div>
   
 </form>
-
+</center>
 </div>
   <!--posts-->
   @foreach ($data as $post)
@@ -46,11 +46,11 @@
         <img src="{{$post->pic_url}}" class="img-responsive" alt="{{$post->pic_url}}">
         
         <div class="caption">
-          <h2><center><span class="glyphicon glyphicon-cutlery"></span> {{$post->restaurant}}　  
-              <span class="glyphicon glyphicon-yen"></span>{{$post->cost}}</center></h2>
+          <h2><center><span class="glyphicon glyphicon-cutlery"></span> {{$post->restaurant}}</center> 
+          <center><span class="glyphicon glyphicon-yen"></span>{{$post->cost}}</center></h2>
             <center>  
             <h2><a href="{{ route('posts.show',['id' => $post->id]) }}"><span class="glyphicon glyphicon-option-horizontal" aria-hidden="true"></span></a>
-            <a href="{{ route('posts.edit',['id' => $post->id]) }}"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>
+            <a href="{{ route('posts.edit',['id' => $post->id]) }}"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a></h2>
             </center>
             <center>
             @include('user_favo.favo_button')
@@ -60,25 +60,28 @@
     </div>
     
     <!--ここからPC表示-->
-    <div class="hidden-xs col-sm-5 col-md-5 col-lg-offset-4 col-lg-4">
-      <div class="PC" id="{{$post->went_at}}">
+    <div class="hidden-xs col-sm-5 col-md- col-lg-offset-3 col-lg-6 thumbnail">
+      <div  id="{{$post->went_at}}">
+        <div class='col-lg-6'>
         <caption style="text-align:right;"><h2>{{$post->went_at}}</h2></caption>
-        <img src="{{$post->pic_url}}" class="img-responsive" alt="{{$post->pic_url}}">
         
-          
-          <div class="caption">
-          <h2><span class="glyphicon glyphicon-cutlery"></span> {{$post->restaurant}}　  
-              <span class="glyphicon glyphicon-yen"></span>{{$post->cost}}</h2>
+        
+        <img src="{{$post->pic_url}}" class="img-responsive" alt="{{$post->pic_url}}">
+        </div>
+          <br>
+          <br>
+          <div class="caption-pc col-lg-6">
+          <h2><span class="glyphicon glyphicon-cutlery"></span> {{$post->restaurant}}</h2>
+          <h2><span class="glyphicon glyphicon-yen"></span>{{$post->cost}}</h2>
               
-            <h2><a href="{{ route('posts.show',['id' => $post->id]) }}"><span class="glyphicon glyphicon-option-horizontal" aria-hidden="true"></span></a>
-            <a href="{{ route('posts.edit',['id' => $post->id]) }}"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>
+            <h2><a href="{{ route('posts.show',['id' => $post->id]) }}"><span class="glyphicon glyphicon-option-horizontal" aria-hidden="true"></span>詳細</a></h2>
+            <h2><a href="{{ route('posts.edit',['id' => $post->id]) }}"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>編集</a></h2>
             
             @include('user_favo.favo_button')
            
           </div>
       </div>
     </div>
-  </div>
   </div>
   @endforeach
   
