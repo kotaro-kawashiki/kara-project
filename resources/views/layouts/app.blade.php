1cbbc0a56d3b57269f9019c075b64b1139f29634
tomohiro.sai <tomohiro.sai@rakuten.com>
2018-07-19 06:27:23 +0000

    complete

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
         <script type="text/javascript" src="/js/jquery-3.3.1.js"></script> 
         
     　　<!--CSS-->
-        <link href="css/style.css" rel="stylesheet" type="text/css">
+        <link href="/css/style.css" rel="stylesheet" type="text/css">
         
     </head>
     <body>
