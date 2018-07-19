654d3d1e08521d5b699e22b13ef1692662d0a26d
tomohiro.sai <tomohiro.sai@rakuten.com>
2018-07-19 04:12:42 +0000

    complete２

--- resources/views/post/favolist.blade.php
+++ resources/views/post/favolist.blade.php
@@ -29,7 +29,7 @@
       </div>
     </div>
   
-  
+  <!--ここからPC表示-->
     <div class="hidden-xs col-sm-5 col-md- col-lg-offset-3 col-lg-6 thumbnail">
       <div  id="{{$post->went_at}}">
         <div class='col-lg-6'>
