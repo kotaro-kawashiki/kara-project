f0505b53c687496cf1c29f0660f58e932154b37b
yusuke.kimura <yusuke.a.kimura@rakuten.com>
2018-07-19 05:57:06 +0000

    syosai

--- resources/views/post/detail.blade.php
+++ resources/views/post/detail.blade.php
@@ -5,29 +5,71 @@
 <div class="col-xs-12 col-sm-5 col-md-5 col-lg-5">
      <img src="{{$post->pic_url}}" class="img-responsive" alt="{{$post->pic_url}}">
      <caption>
-          <a href="{{ route('posts.edit',['id' => $post->id]) }}"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>
-          <a href="#"><span class="glyphicon glyphicon-heart"></span></a>
-     </caption>
+         
+     </caption> 
 </div>
 <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
-     <ul class="list-group list-group-flush">
-       <li class="list-group-item"><h2>{{$post->restaurant}}</h2></li>
-       <li class="list-group-item"><h3>{{$post->cost}}円</h3></li>
-       <li class="list-group-item"><ul style="padding-left:0px;">
-          @foreach($peoples as $people)
-               @if($people->post_id==$post->id)
-                    <li style="display:inline-block;">{{$people->people_name}},</li>
-               @endif
-          @endforeach
-          </ul></li>
-       <li class="list-group-item">{{$post->went_at}}</li>
-       <li class="list-group-item">{{$post->comments}}</li>
-     </ul>
-     @if (Auth::user()->id == $post->user_id)
-        {!! Form::open(['route' => ['posts.destroy', $post->id], 'method' => 'delete']) !!}
-            {!! Form::submit('なかった事にする', ['class' => 'btn btn-danger btn-xs pull-right']) !!}
-        {!! Form::close() !!}
-     @endif
+     
+     <div class="panel panel-default">
+            <div class="panel-heading">店名</div>
+            <div class="panel-body">
+              <h4>{{$post->restaurant}}</h4>
+            </div>
+            <div class="panel-heading">金額</div>
+            <div class="panel-body">
+              {{$post->cost}}円
+            </div>
+            <div class="panel-heading">同行者</div>
+            <div class="panel-body">
+                  @foreach($peoples as $people)
+                        @if($people->post_id==$post->id)
+                            {{$people->people_name}},
+                        @endif
+              　　@endforeach
+            </div>
+            <div class="panel-heading">日付</div>
+            <div class="panel-body">
+              {{$post->went_at}}
+            </div>
+            <div class="panel-heading">メモ</div>
+            <div class="panel-body">
+              {{$post->comments}}
+            </div>
+        </div>
+          
+        <div class="btn-toolbar" role="toolbar">
+            @if (Auth::user()->id == $post->user_id)
+                {!! Form::open(['route' => ['posts.destroy', $post->id], 'method' => 'delete']) !!}
+                    {!! Form::submit('なかった事にする', ['class' => 'btn btn-danger btn-lg']) !!}
+                {!! Form::close() !!}
+            @endif
+            <div class="btn-group" role="group">
+                @include('user_favo.favo_button')
+            </div>
+            
+            <div class="btn-group" role="group">
+              {!! link_to_route('posts.edit', 'この投稿を編集', ['id' => $post->id], ['class' => 'btn btn-success btn-lg']) !!}
+            </div>
+        </div>
 </div>
+     
+     
+     <!--<table border="1">-->
+     <!--     <tbody>-->
+     <!--            <tr><td><h4>店名</h4></td><td><h3>{{$post->restaurant}}</h3></td></tr>-->
+     <!--            <tr><td><h4>金額</h4></td><td><h3>{{$post->cost}}円</h3></td></tr>-->
+     <!--            <tr><td><h4>同行者</h4></td>-->
+     <!--            <td><h3>-->
+     <!--               @foreach($peoples as $people)-->
+     <!--                    @if($people->post_id==$post->id)-->
+     <!--                         {{$people->people_name}},-->
+     <!--                    @endif-->
+     <!--               @endforeach-->
+     <!--               </h3></td></tr>-->
+     <!--            <tr><td><h4>日付</h4></td><td><h3>{{$post->went_at}}</h3></td></tr>-->
+     <!--            <tr><td><h4>メモ</h4></td><td><h3>{{$post->comments}}</h3></td></tr>-->
+     <!--     </tbody>-->
+     <!--</table>-->
+
 
 @endsection
