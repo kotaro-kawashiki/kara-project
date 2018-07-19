869cd9a39be1c483c87bd1ff671afcdec660bcdf
yusuke.kimura <yusuke.a.kimura@rakuten.com>
2018-07-19 07:41:29 +0000

    syosaifinish

--- resources/views/post/detail.blade.php
+++ resources/views/post/detail.blade.php
@@ -38,26 +38,28 @@
             </div>
         </div>
           
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8">
         <div class="btn-toolbar" role="toolbar">
-            <div class="col-xs-12 col-sm-2 col-md-2 col-lg-2">
+            <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
                 @if (Auth::user()->id == $post->user_id)
                     {!! Form::open(['route' => ['posts.destroy', $post->id], 'method' => 'delete']) !!}
                         {!! Form::submit('なかった事にする', ['class' => 'btn btn-danger']) !!}
                     {!! Form::close() !!}
                 @endif
             </div>
-            <div class="col-xs-12 col-sm-2 col-md-2 col-lg-2">
+            <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
                 <div class="btn-group" role="group"　class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
                     @include('user_favo.favo_button')
                 </div>
             </div>
             
-            <div class="col-xs-12 col-sm-2 col-md-2 col-lg-2">
+            <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
                 <div class="btn-group" role="group" class="col-xs-12 col-sm-2 col-md-2 col-lg-2">
                   {!! link_to_route('posts.edit', 'この投稿を編集', ['id' => $post->id], ['class' => 'btn btn-success']) !!}
                 </div>
             </div>
         </div>
+    </div>
 </div>
      
      
