97963df591a9e29c9a60d70df38d1f4cf1c1bc08
kotaro.kawashiki <kotaro.kawashiki@rakuten.com>
2018-07-24 23:30:09 +0000

    ログインと新規登録画面の調整とモーダル追加

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,19 +14,14 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <br>
-            <br>
             <div class='panel'>
                 <center><h2>ログイン</h2></center>
-                <br>
                 <center><img src="/image/icon.jpg" alt="/image/icon.jpg"></center>
-                <br>
-                <br>
                 <div>
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}" style="padding:10px;">
                         {{ csrf_field() }}
                         <div class="form-group{{ $errors->has('userid') ? ' has-error' : '' }}">
-                            <div class="col-lg-offset-3 col-lg-6 col-md-6">
+                            <div class="col-lg-offset-3 col-lg-6 col-md-6 col-md-offset-3">
                                 <input id="userid" type="userid" class="form-control" name="userid" value="{{ old('userid') }}" placeholder="ユーザーid" required autofocus>
                                 @if ($errors->has('userid'))
                                     <span class="help-block">
@@ -36,7 +31,7 @@
                             </div>
                         </div>
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <div class="col-lg-offset-3 col-lg-6 col-md-6">
+                            <div class="col-lg-offset-3 col-lg-6 col-md-6 col-md-offset-3">
                                 <input id="password" type="password" class="form-control" name="password" placeholder="パスワード" required>
                                 @if ($errors->has('password'))
                                     <span class="help-block">
@@ -46,18 +41,44 @@
                             </div>
                         </div>
                         <div class="form-group">
-                            <center><div class="form-group">
-                            <div class="col-md-6 col-md-offset-4 col-lg-offset-5 col-lg-2">
-                                <button type="submit" class="btn btn-info">
-                                    ログイン
-                                </button>
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-3 col-lg-offset-5 col-lg-2">
+                                    <center>
+                                        <button type="submit" class="btn btn-info">
+                                            ログイン
+                                        </button>
+                                    </center>
+                                </div>
                             </div>
-                        </div></center>
                         </div>
                     </form>
+                    <center>
+                        <a data-toggle="modal" data-target="#exampleModal">モバイル版はこちら</a>
+                    </center>
                 </div>
             </div>
         </div>
     </div>
+</div>
+
+<!-- QRコード表示用モーダル -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,28 +15,16 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <br>
-            <br>
-            <br>
                <div class='panel'>
                 <center><h2>新規登録</h2></center>
-                <br>
                 <center><img src="/image/icon2.jpg" alt="/image/icon2.jpg"></center>
-                <br>
-                <br>
-
                 <div>
-
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
-
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-
-
-                            <div class="col-lg-offset-3 col-lg-6 col-md-6">
+                            <div class="col-lg-offset-3 col-lg-6 col-md-6 col-md-offset-3">
                                 <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="ユーザー名"required autofocus>
-
                                 @if ($errors->has('name'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('name') }}</strong>
@@ -44,13 +32,9 @@
                                 @endif
                             </div>
                         </div>
-
                         <div class="form-group{{ $errors->has('userid') ? ' has-error' : '' }}">
-                            
-
-                            <div class="col-lg-offset-3 col-lg-6 col-md-6">
+                            <div class="col-lg-offset-3 col-lg-6 col-md-6 col-md-offset-3">
                                 <input id="userid" type="userid" class="form-control" name="userid" value="{{ old('userid') }}" placeholder="ユーザーid"required>
-
                                 @if ($errors->has('userid'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('userid') }}</strong>
@@ -58,12 +42,9 @@
                                 @endif
                             </div>
                         </div>
-
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            
-                            <div class="col-lg-offset-3 col-lg-6 col-md-6">
+                            <div class="col-lg-offset-3 col-lg-6 col-md-6 col-md-offset-3">
                                 <input id="password" type="password" class="form-control" name="password" placeholder="パスワード"required>
-
                                 @if ($errors->has('password'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
@@ -71,25 +52,48 @@
                                 @endif
                             </div>
                         </div>
-
                         <div class="form-group">
-
-                            <div class="col-lg-offset-3 col-lg-6 col-md-6">
+                            <div class="col-lg-offset-3 col-lg-6 col-md-6 col-md-offset-3">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="パスワード(確認用）"required>
                             </div>
                         </div>
-
-                        <center><div class="form-group">
-                            <div class="col-md-6 col-md-offset-4 col-lg-offset-5 col-lg-2">
-                                <button type="submit" class="btn btn-info">
-                                    登録
-                                </button>
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-3 col-lg-offset-5 col-lg-2">
+                                <center>
+                                    <button type="submit" class="btn btn-info">
+                                        登録
+                                    </button>
+                                </center>
                             </div>
-                        </div></center>
+                        </div>
                     </form>
+                    <center>
+                        <a data-toggle="modal" data-target="#exampleModal">モバイル版はこちら</a>
+                    </center>
                 </div>
             </div>
         </div>
     </div>
+</div>
+
+<!-- QRコード表示用モーダル -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
